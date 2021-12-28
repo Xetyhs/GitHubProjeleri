55c36e2c5951007ec4ff1a3156c27ba57509afc0
Xetyhs <yigicruelsun.tf@gmail.com>
2021-12-28 22:12:41 +0300

    C#101 Console Programlama
    Git push for C#101 Console Programming section.

---
+++ CSharp101Projects/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSharp101Projects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C#101 Eğitimi Başlasın");
+            string input = Console.ReadLine();
+            Console.WriteLine(input);
+        }
+    }
+}
